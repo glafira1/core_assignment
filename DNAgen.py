1f6a6caef24b42681608c32cdc8357b7fc4cf5a1
glafira1 <100717544+glafira1@users.noreply.github.com>
2022-03-08 12:53:56 -0500

    python file

---
+++ DNAgen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 17:18:38 2022
+
+@author: glafiraermakova
+"""
+
+#Part A:
+#Generating FASTA file containing randomly generated sequences
